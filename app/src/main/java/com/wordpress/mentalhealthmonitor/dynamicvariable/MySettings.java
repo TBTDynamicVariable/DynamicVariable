2638801d0278ce74f7c27a17efc772fe176d7c3c
Myles Lewis <mglewis@bu.edu>
2018-12-07 12:52:55 -0500

    Adding buttons for volume on and off, also initialized variables and made the skeleton functions for those operations

--- app/src/main/java/com/wordpress/mentalhealthmonitor/dynamicvariable/MySettings.java
+++ app/src/main/java/com/wordpress/mentalhealthmonitor/dynamicvariable/MySettings.java
@@ -8,12 +8,33 @@ import android.widget.Button;
 
 public class MySettings extends AppCompatActivity {
 
+    public Button VolumeOnButton;
+    public Button VolumeOffButton;
+
     private Button toMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_settings);
+
+        VolumeOnButton = (Button) findViewById(R.id.VolumeOn);
+        VolumeOffButton = (Button) findViewById(R.id.VolumeOff);
         toMain = (Button) findViewById(R.id.mainButSettings);
+
+        VolumeOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnVolumeOff();
+                // This will be the function to turn off the button
+            }
+        });
+        VolumeOnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnVolumeOn();
+                // THis turns the volume on, it wull probably have to error check and see if the volume is slready on
+            }
+        });
 
         toMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +47,11 @@ public class MySettings extends AppCompatActivity {
     }
     public void openMain(){
        finish();
+    }
+    public void turnVolumeOn(){
+        //This turns the volume on
+    }
+    public void turnVolumeOff(){
+        //Thus turns the volume off
     }
 }
